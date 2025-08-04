95e1f016219c1cdd6783eb75b15aacb9d369d252
HoTung04 <tunghvgcs220222@fpt.edu.vn>
2025-08-04 12:54:12 +0700

    Full3Task.py

--- Full3Task.py
+++ Full3Task.py
@@ -1,240 +1,238 @@
 import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
+import networkx as nwx
+import matplotlib.pyplot as matplt
+import matplotlib.lines as matline
 
 # For Task1 + Task2
 
-# --- Piccadilly line ---
+# --- Piccadilly Route ---
 stations1 = [
-    'Hyde Park Corner',
-    'Green Park',
-    'Piccadilly Circus',
-    'Leicester Square',
-    'Covent Garden',
-    'Holborn'
+    'Holborn Station',
+    'Covent Garden Station',
+    'Leicester Square Station',
+    'Piccadilly Circus Station',
+    'Green Park Station',
+    'Hyde Park Corner Station'
 ]
 
 edges1 = [
-    ('Hyde Park Corner', 'Green Park', 0.8),#784.36
-    ('Green Park', 'Piccadilly Circus', 0.7),#696.10
-    ('Piccadilly Circus', 'Leicester Square', 0.45),#445.98
-    ('Leicester Square', 'Covent Garden', 0.3),#324.85
-    ('Covent Garden', 'Holborn', 0.6)#570.37
+    ('Leicester Square Station', 'Covent Garden Station', 0.3),#324.85
+    ('Piccadilly Circus Station', 'Leicester Square Station', 0.45),#445.98
+    ('Covent Garden Station', 'Holborn Station', 0.6),#570.37
+    ('Green Park Station', 'Piccadilly Circus Station', 0.7),#696.10
+    ('Hyde Park Corner Station', 'Green Park Station', 0.8),#784.36
 ]
 
-# ---Central line ---
+# ---Central Route ---
 stations2 = [
-    'Queensway',
-    'Lancaster Gate',
-    'Marble Arch',
-    'Bond Street',
-    'Oxford Circus',
-    'Holborn'
+    'Holborn Station',
+    'Oxford Circus Station',
+    'Bond Street Station',
+    'Marble Arch Station',
+    'Lancaster Gate Station',
+    'Queensway Station'
 ]
 
 edges2 = [
-    ('Queensway', 'Lancaster Gate', 0.8),#832.66
-    ('Lancaster Gate', 'Marble Arch', 1.2),#1.19
-    ('Marble Arch', 'Bond Street', 0.7),#664.26
-    ('Bond Street', 'Oxford Circus', 0.5),#493.74
-    ('Oxford Circus', 'Holborn', 1.5)#1.53
+    ('Bond Street Station', 'Oxford Circus Station', 0.5),#493.74
+    ('Marble Arch Station', 'Bond Street Station', 0.7),#664.26
+    ('Queensway Station', 'Lancaster Gate Station', 0.8),#832.66
+    ('Lancaster Gate Station', 'Marble Arch Station', 1.2),#1.19
+    ('Oxford Circus Station', 'Holborn Station', 1.5)#1.53
 ]
-# --- Metropolitan line ---
+# --- Metropolitan Route ---
 stations3 = [
-    'Marylebone',
-    'Baker Street',
-    'Regent’s Park',
-    'Oxford Circus',
-    'Piccadilly Circus',
-    'Charing Cross'
+    'Charing Cross Station',
+    'Piccadilly Circus Station',
+    'Oxford Circus Station',
+    'Regent’s Park Station',
+    'Baker Street Station',
+    'Marylebone Station',
 ]
 
 edges3 = [
-    ('Marylebone', 'Baker Street', 0.4),#420.97
-    ('Baker Street', 'Regent’s Park', 0.7),#703.88
-    ('Regent’s Park', 'Oxford Circus', 1.0),#990.41
-    ('Oxford Circus', 'Piccadilly Circus', 0.8),#777.88
-    ('Piccadilly Circus', 'Charing Cross', 0.6)#602.47
+    ('Marylebone Station', 'Baker Street Station', 0.4),#420.97
+    ('Piccadilly Circus Station', 'Charing Cross Station', 0.6),#602.47
+    ('Baker Street Station', 'Regent’s Park Station', 0.7),#703.88
+    ('Oxford Circus Station', 'Piccadilly Circus Station', 0.8),#777.88
+    ('Regent’s Park Station', 'Oxford Circus Station', 1.0),#990.41
 ]
-# ---Victoria line ---
+# ---Victoria Route ---
 stations4 = [
-    'Victoria',
-    'Green Park',
-    'Oxford Circus',
-    'Warren Street',
-    'Euston',
-    'King’s Cross & St Pancras Internation'
+    'King’s Cross & St Pancras Internation Station',
+    'Euston Station',
+    'Victoria Station',
+    'Green Park Station',
+    'Oxford Circus Station',
+    'Warren Street Station',
 ]
 
 edges4 = [
-    ('Victoria', 'Green Park', 1.1),#1.12
-    ('Green Park', 'Oxford Circus', 0.9),#924
-    ('Oxford Circus', 'Warren Street', 1.1),#1.08
-    ('Warren Street', 'Euston', 0.5),#503.22
-    ('Euston', 'King’s Cross & St Pancras Internation', 0.7)#764.98
+    ('Warren Street Station', 'Euston Station', 0.5),#503.22
+    ('Euston Station', 'King’s Cross & St Pancras Internation Station', 0.7),#764.98
+    ('Green Park Station', 'Oxford Circus Station', 0.9),#924
+    ('Oxford Circus Station', 'Warren Street Station', 1.1),#1.08
+    ('Victoria Station', 'Green Park Station', 1.1),#1.12
 ]
 
 
 # --- Graph ---
-G = nx.Graph()
-for u, v, d in edges1 + edges2 + edges3 + edges4:
-    G.add_edge(u, v, weight=d)
+Graph = nwx.Graph()
+for x, y, z in edges1 + edges2 + edges3 + edges4:
+    Graph.add_edge(x, y, weight=z)
 
 # --- Dot position( x, y) ---
-pos = {
-    # Piccadilly line
-    'Hyde Park Corner': (-2.7, -1.6),
-    'Green Park': (-1.125, -1.2),
-    'Piccadilly Circus': (0.1, -1.2),
-    'Leicester Square': (1.1, -1.2),
-    'Covent Garden': (2, -1),
-    'Holborn': (3, -0.375),
-    # Central line
-    'Queensway': (-6.3, -1),
-    'Lancaster Gate': (-4.7, -0.8),
-    'Marble Arch': (-2.3, -0.375),
-    'Bond Street': (-1, 0),
-    'Oxford Circus': (0, 0),
-    'Holborn': (3, -0.375),
-    # Metropolitan line
-    'Marylebone': (-3.3, 2.3),
-    'Baker Street': (-2.65, 1.875),
-    'Regent’s Park': (-1.125, 1.5),
-    'Oxford Circus': (0, 0),
-    'Piccadilly Circus': (0.1, -1.2),
-    'Charing Cross': (1.1, -1.6),
-    # Victoria line
-    'Victoria': (-1.125, -3.35),
-    'Green Park': (-1.125, -1.2),
-    'Oxford Circus': (0, 0),
-    'Warren Street': (1.1, 1.875),
-    'Euston': (2, 2.3),
-    'King’s Cross & St Pancras Internation': (3.5, 2.3),
+dot = {
+    # Piccadilly Route
+    'Holborn Station': (3, -0.375),
+    'Covent Garden Station': (2, -1),
+    'Leicester Square Station': (1.1, -1.2),
+    'Piccadilly Circus Station': (0.1, -1.2),
+    'Green Park Station': (-1.125, -1.2),
+    'Hyde Park Corner Station': (-2.7, -1.6),
+    # Central Route
+    'Holborn Station': (3, -0.375),
+    'Oxford Circus Station': (0, 0),
+    'Bond Street Station': (-1, 0),
+    'Marble Arch Station': (-2.3, -0.375),
+    'Lancaster Gate Station': (-4.7, -0.8),
+    'Queensway Station': (-6.3, -1),
+    # Metropolitan Route
+    'Charing Cross Station': (1.1, -1.6),
+    'Piccadilly Circus Station': (0.1, -1.2),
+    'Oxford Circus Station': (0, 0),
+    'Regent’s Park Station': (-1.125, 1.5),
+    'Baker Street Station': (-2.65, 1.875),
+    'Marylebone Station': (-3.3, 2.3),
+    # Victoria Route
+    'King’s Cross & St Pancras Internation Station': (3.5, 2.3),
+    'Euston Station': (2, 2.3),
+    'Warren Street Station': (1.1, 1.875),
+    'Green Park Station': (-1.125, -1.2),
+    'Victoria Station': (-1.125, -3.35),
+    'Oxford Circus Station': (0, 0),
 }
 
 # --- Offset label position ---
 offset = {
-    # Piccadilly line
-    'Hyde Park Corner': (-0.1, -0.4),
-    'Green Park': (0, -0.4),
-    'Piccadilly Circus': (0, -0.4),
-    'Leicester Square': (0, 0.4),
-    'Covent Garden': (0, -0.4),
-    'Holborn': (0,-0.4),
-    # Central line
-    'Queensway': (0, -0.4),
-    'Lancaster Gate': (0, -0.4),
-    'Marble Arch': (0, -0.4),
-    'Bond Street': (0, -0.4),
-    'Oxford Circus': (0, 0.4),
-    'Holborn': (0, -0.4),
-    # Metropolitan line
-    'Marylebone': (0, -0.4),
-    'Baker Street': (0, -0.4),
-    'Regent’s Park': (0, 0.4),
-    'Oxford Circus': (0, 0.4),
-    'Piccadilly Circus': (0, -0.4),
-    'Charing Cross': (0, -0.4),
-    # Victoria line
-    'Victoria': (0, -0.4),
-    'Green Park': (0, -0.4),
-    'Oxford Circus': (0, 0.4),
-    'Warren Street': (0, -0.4),
-    'Euston': (0, -0.4),
-    'King’s Cross & St Pancras Internation': (0, -0.4),
+    # Piccadilly Route
+    'Green Park Station': (0, -0.4),
+    'Covent Garden Station': (0, -0.4),
+    'Hyde Park Corner Station': (-0.1, -0.4),
+    'Piccadilly Circus Station': (0, -0.4),
+    'Leicester Square Station': (0, 0.4),
+    'Holborn Station': (0,-0.4),
+    # Central Route
+    'Lancaster Gate Station': (0, -0.4),
+    'Bond Street Station': (0, -0.4),
+    'Holborn Station': (0, -0.4),
+    'Queensway Station': (0, -0.4),
+    'Marble Arch Station': (0, -0.4),
+    'Oxford Circus Station': (0, 0.4),
+    # Metropolitan Route
+    'Baker Street Station': (0, -0.4),
+    'Oxford Circus Station': (0, 0.4),
+    'Charing Cross Station': (0, -0.4),
+    'Marylebone Station': (0, -0.4),
+    'Regent’s Park Station': (0, 0.4),
+    'Piccadilly Circus Station': (0, -0.4),
+    # Victoria Route
+    'Green Park Station': (0, -0.4),
+    'Warren Street Station': (0, -0.4),
+    'King’s Cross & St Pancras Internation Station': (0, -0.4),
+    'Victoria Station': (0, -0.4),
+    'Oxford Circus Station': (0, 0.4),
+    'Euston Station': (0, -0.4),
 }
 
 # --- Calculate new coordinates for label ---
-offset_pos = {node: (pos[node][0] + dx, pos[node][1] + dy)
-              for node, (dx, dy) in offset.items()}
+offset_dot = {nd: (dot[nd][0] + dx, dot[nd][1] + dy)
+              for nd, (dx, dy) in offset.items()}
 
 # --- Get distance label ---
-edge_labels = nx.get_edge_attributes(G, 'weight')
+edge_labels = nwx.get_edge_attributes(Graph, 'weight')
 
 # --- Draw graph ---
-plt.figure(figsize=(12, 6))
+matplt.figure(figsize=(12, 6))
 
 # Draw dot
-nx.draw_networkx_nodes(G, pos,
+nwx.draw_networkx_nodes(Graph, dot,
                        nodelist=stations1,
-                       node_color='darkblue',
-                       node_size=800)
-nx.draw_networkx_nodes(G, pos,
+                       node_size=800,
+                       node_color='darkblue')
+nwx.draw_networkx_nodes(Graph, dot,
                        nodelist=stations2,
-                       node_color='red',
-                       node_size=800)
-nx.draw_networkx_nodes(G, pos,
+                       node_size=800,
+                       node_color='red')
+nwx.draw_networkx_nodes(Graph, dot,
                        nodelist=stations3,
-                       node_color='brown',
-                       node_size=800)
-nx.draw_networkx_nodes(G, pos,
+                       node_size=800,
+                       node_color='brown')
+nwx.draw_networkx_nodes(Graph, dot,
                        nodelist=stations4,
-                       node_color='blue',
-                       node_size=800)
+                       node_size=800,
+                       node_color='blue')
 
-# draw line
-nx.draw_networkx_edges(G, pos,
+# draw Route
+nwx.draw_networkx_edges(Graph, dot,
                        edgelist=edges1,
-                       edge_color='darkblue',
-                       width=2)
-nx.draw_networkx_edges(G, pos,
-                       edgelist=edges2,
-                       edge_color='red',
-                       width=2)
-nx.draw_networkx_edges(G, pos,
+                       width=2,
+                       edge_color='darkblue')
+nwx.draw_networkx_edges(Graph, dot,
+                       edgelist=edges2, 
+                       width=2,
+                       edge_color='red')
+nwx.draw_networkx_edges(Graph, dot,
                        edgelist=edges3,
-                       edge_color='brown',
-                       width=2)
-nx.draw_networkx_edges(G, pos,
+                       width=2,
+                       edge_color='brown')
+nwx.draw_networkx_edges(Graph, dot,
                        edgelist=edges4,
-                       edge_color='blue',
-                       width=2)
+                       width=2,
+                       edge_color='blue')
 
-# line label
-nx.draw_networkx_edge_labels(
-    G, pos,
-    edge_labels={k: f"{v:.1f} km" for k, v in edge_labels.items()},
-    font_size=8
+# Route label
+nwx.draw_networkx_edge_labels(
+    Graph, dot,
+    edge_labels={a: f"{y:.1f} km" for a, y in edge_labels.items()},
+    font_size=7
 )
 
 # station label
-nx.draw_networkx_labels(G, pos,
-                        labels={n: "" for n in G.nodes()},
-                        font_size=9)
-nx.draw_networkx_labels(G, offset_pos,
-                        font_size=9,
-                        font_family="sans-serif")
+nwx.draw_networkx_labels(Graph, dot,
+                        labels={n: "" for n in Graph.nodes()},
+                        font_size=8)
+nwx.draw_networkx_labels(Graph, offset_dot,
+                        font_size=8,
+                        font_family="arial")
 
-# Line annotation
-piccadilly_line = mlines.Line2D([], [], color='darkblue', label='Piccadilly Line')
-central_line = mlines.Line2D([], [], color='red', label='Central Line')
-metropolitan_line = mlines.Line2D([], [], color='brown', label='Metropolitan Line')
-victoria_line = mlines.Line2D([], [], color='blue', label='Victoria Line')
-plt.legend(handles=[piccadilly_line, central_line,metropolitan_line,victoria_line], loc='lower right',
-           title="Lines", frameon=True)
+# Route annotation
+piccadilly_route = matline.Line2D([], [], color='darkblue', label='Piccadilly Route')
+central_route = matline.Line2D([], [], color='red', label='Central Route')
+metropolitan_route = matline.Line2D([], [], color='brown', label='Metropolitan Route')
+victoria_route = matline.Line2D([], [], color='blue', label='Victoria Route')
+matplt.legend(handles=[piccadilly_route, central_route,metropolitan_route,victoria_route], loc='lower left',
+           title="Routes", frameon=True)
 
-plt.title(" Transport Map", fontsize=14)
-plt.axis('off')
-plt.tight_layout()
-plt.show()
+matplt.title(" Map of The transportation system", fontsize=14)
+matplt.show()
 
 # Task 3
 
-# Get all edge distances
-edge_weights = list(nx.get_edge_attributes(G, 'weight').values())
+# Acquire all edge values(distance)
+edge_weights = list(nwx.get_edge_attributes(Graph, 'weight').values())
 
-# Total length of the network
-total_length = sum(edge_weights)
+# Total network length
+nw_total_length = sum(edge_weights)
 
-# Average distance between stations
-average_distance = np.mean(edge_weights)
+# Average separation between stations
+stt_average_distance = np.mean(edge_weights)
 
-# Standard deviation of distances
-std_deviation = np.std(edge_weights)
+# The gap standard deviation
+stt_std_deviation = np.std(edge_weights)
 
 # Print results 
-print("=== Transport Network Statistics ===")
-print(f"Total length of the network         : {total_length:.2f} km")
-print(f"Average distance between stations  : {average_distance:.2f} km")
-print(f"Standard deviation of distances    : {std_deviation:.2f} km")
+print("=== Metrics from Transport Networks ===")
+print(f"Total network length : {nw_total_length} kilometer")
+print(f"Average separation between stations(distance) : {stt_average_distance} kilometer")
+print(f"The gap standard deviation : {stt_std_deviation} kilometer")
